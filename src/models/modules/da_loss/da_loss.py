1fd05b5ca83107bc4ca79ea831d4a51b50c05a62
Eoghan <eoghan@deepseek.ai>
2023-04-26 13:48:41 +0100

    add da loss modules

---
+++ src/models/modules/da_loss/da_loss.py
@@ -0,0 +1,69 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from src.models.modules.da_loss.consistency_loss import consistency_loss
+
+class DALossComputation(object):
+    """
+    This class computes the DA loss.
+    """
+
+    def __init__(self):
+        resolution = 7
+        self.avgpool = nn.AvgPool2d(kernel_size=resolution, stride=resolution)
+        
+    def prepare_masks(self, targets):
+        masks = []
+        for targets_per_image in targets:
+            is_source = targets_per_image['is_source']
+            mask_per_image = is_source.new_ones(1, dtype=torch.uint8) if is_source.any() else is_source.new_zeros(1, dtype=torch.uint8)
+            masks.append(mask_per_image)
+        return masks
+
+    def __call__(self, da_img, da_ins, da_img_consist, da_ins_consist, da_ins_labels, targets):
+        """
+        Arguments:
+            da_img (list[Tensor])
+            da_img_consist (list[Tensor])
+            da_ins (Tensor)
+            da_ins_consist (Tensor)
+            da_ins_labels (Tensor)
+            targets (list[BoxList])
+
+        Returns:
+            da_img_loss (Tensor)
+            da_ins_loss (Tensor)
+            da_consist_loss (Tensor)
+        """
+
+        masks = self.prepare_masks(targets)
+        masks = torch.cat(masks, dim=0)
+
+        da_img_flattened = []
+        da_img_labels_flattened = []
+
+        for da_img_per_level in da_img:
+            N, A, H, W = da_img_per_level.shape
+            da_img_per_level = da_img_per_level.permute(0, 2, 3, 1)
+            da_img_label_per_level = torch.zeros_like(da_img_per_level, dtype=torch.float32)
+            da_img_label_per_level[masks, :] = 1
+
+            da_img_per_level = da_img_per_level.reshape(N, -1)
+            da_img_label_per_level = da_img_label_per_level.reshape(N, -1)
+            
+            da_img_flattened.append(da_img_per_level)
+            da_img_labels_flattened.append(da_img_label_per_level)
+            
+        da_img_flattened = torch.cat(da_img_flattened, dim=0)
+        da_img_labels_flattened = torch.cat(da_img_labels_flattened, dim=0)
+        
+        da_img_loss = F.binary_cross_entropy_with_logits(
+            da_img_flattened, da_img_labels_flattened
+        )
+        da_ins_loss = F.binary_cross_entropy_with_logits(
+            torch.squeeze(da_ins), da_ins_labels.type(torch.cuda.FloatTensor)
+        )
+        da_consist_loss = consistency_loss(da_img_consist, da_ins_consist, da_ins_labels, size_average=True)
+
+        return da_img_loss, da_ins_loss, da_consist_loss
